29abe42fbd00be4ca26dfa3b9487c296f8dcbd45
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-13 21:56:22 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
-/*
+
 $servername = "corporate.viplus1.noc401.com";
 $username = "parokyan_parokyan";
 $password = "l)UKks28.Jp67H";
-$dbname = "parokyan_san-gregorio-db"; */
-
+$dbname = "parokyan_san-gregorio-db"; 
+/*
 $servername = "localhost";
 $username = "parokyan";
 $password = "l)UKks28.Jp67H";
-$dbname = "san-gregorio-db";
+$dbname = "san-gregorio-db";*/
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
