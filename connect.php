83d19b1d5b31cc395ebd386ffd7a4951c6102134
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-14 01:06:55 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,16 @@
 <?php
-/*
+
 $servername = "corporate.viplus1.noc401.com";///162.213.210.29
 $username = "parokyan_parokyan";
 $password = "l)UKks28.Jp67H";
 $dbname = "parokyan_san_gregorio_db"; 
-*/
+/*
 $servername = "localhost";
 $username = "parokyan";
 $password = "l)UKks28.Jp67H";
 $dbname = "san-gregorio-db";
 
-
+*/
 
 
 // Create connection
