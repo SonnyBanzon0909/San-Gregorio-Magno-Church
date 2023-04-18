e89225bedf1961145bceee8fa98d5f43289a288d
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-18 11:23:20 +0800

    Update navigation.php

--- editor/partial/navigation.php
+++ editor/partial/navigation.php
@@ -81,7 +81,7 @@
           <div class="button-overlay"></div>
           <a aria-label="" href="includes/logout.php" class="button-link">Logout</a>
         </div>
-        <div data-hover="false" data-delay="500" data-w-id="a3e29982-56b0-b8b9-18b8-0f02a2630aa4" class="dropdown w-dropdown profile">
+        <div data-hover="false" data-delay="500" id="#profile" data-w-id="a3e29982-56b0-b8b9-18b8-0f02a2630aa4" class="dropdown w-dropdown profile">
           <div class="dropdown-toggle w-dropdown-toggle">
             <div class="profile-img-con" style="margin-right: 23px;">
               <div class="profile-initial" style="text-transform: uppercase;">E</div>
