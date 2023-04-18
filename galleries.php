2f2b941a5ae1eb8b2399a814b8e1049797624acf
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-19 00:27:52 +0800

    Update galleries.php

--- galleries.php
+++ galleries.php
@@ -29,7 +29,7 @@ else
 
 
 
-    $query = "SELECT * FROM gallery_tbl WHERE id = ".$id.";
+    $query = "SELECT * FROM gallery_tbl WHERE id = ".$id;
     $view_query = mysqli_query($connection, $query);
 
     if(!$view_query){
