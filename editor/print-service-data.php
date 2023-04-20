5bc649e59ec4a84ec4e5dd158674d3c4c614cfa0
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-20 11:53:51 +0800

    Update print-service-data.php

--- editor/print-service-data.php
+++ editor/print-service-data.php
@@ -1,20 +1,25 @@
 <?php
   include "includes/db.php";
 
-// fetch data from the database
-$sql = "SELECT name, gender, barangay, age, date FROM formdata";
-$result = $conn->query($sql);
+  try {
+    // fetch data from the database
+    $sql = "SELECT name, gender, barangay, age, date FROM formdata";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data as JSON
-  $data = array();
-  while($row = $result->fetch_assoc()) {
-    $data[] = $row;
+    if ($result->num_rows > 0) {
+      // output data as JSON
+      $data = array();
+      while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+      }
+      echo json_encode($data);
+    } else {
+      echo "0 results";
+    }
+
+    $conn->close();
+
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
   }
-  echo json_encode($data);
-} else {
-  echo "0 results";
-}
-
-$conn->close();
 ?>
