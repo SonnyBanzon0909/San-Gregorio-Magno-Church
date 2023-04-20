744cfd22c2a07f63f1155e0215663f0526f339b6
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-20 12:06:24 +0800

    Update print-service-data.php

--- editor/print-service-data.php
+++ editor/print-service-data.php
@@ -1,25 +1,26 @@
 <?php
-  include "includes/db.php";
+  //include "includes/db.php";
 
-  try {
-    // fetch data from the database
-    $sql = "SELECT name, gender, barangay, age, date FROM formdata";
-    $result = $conn->query($sql);
+echo json_encode("DITO SAY");
+  // try {
+  //   // fetch data from the database
+  //   $sql = "SELECT name, gender, barangay, age, date FROM formdata";
+  //   $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-      // output data as JSON
-      $data = array();
-      while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-      }
-      echo json_encode($data);
-    } else {
-      echo "0 results";
-    }
+  //   if ($result->num_rows > 0) {
+  //     // output data as JSON
+  //     $data = array();
+  //     while($row = $result->fetch_assoc()) {
+  //       $data[] = $row;
+  //     }
+  //     echo json_encode($data);
+  //   } else {
+  //     echo "0 results";
+  //   }
 
-    $conn->close();
+  //   $conn->close();
 
-  } catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-  }
+  // } catch (Exception $e) {
+  //   echo "Error: " . $e->getMessage();
+  // }
 ?>
