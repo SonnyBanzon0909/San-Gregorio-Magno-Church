3083151963fab3ea64371fc4afa16daf8bff0fea
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-20 10:52:59 +0800

    Update update_new_service.php

--- editor/includes/update_new_service.php
+++ editor/includes/update_new_service.php
@@ -4,65 +4,68 @@
 	if(isset($_POST['update_service'])){
 		$id = $_POST['id'];
 		$title = $_POST['title'];
-        $content = $_POST['content'];
-        $button_text = $_POST['button_text'];
-        $button_link = $_POST['button_link'];
-        $status = $_POST['status'];
+		$content = $_POST['content'];
+		$button_text = $_POST['button_text'];
+		$button_link = $_POST['button_link'];
+		$status = $_POST['status'];
 
-        $icon = $_FILES['icon']['name'];
-        $icon_tmp = $_FILES['icon']['tmp_name'];
+		$icon = $_FILES['icon']['name'];
+		$icon_tmp = $_FILES['icon']['tmp_name'];
 
-        $photo = $_FILES['photo']['name'];
-        $photo_tmp = $_FILES['photo']['tmp_name'];
+		$photo = $_FILES['photo']['name'];
+		$photo_tmp = $_FILES['photo']['tmp_name'];
 
-        //$post_comment_count = 4;
+		move_uploaded_file($icon_tmp, "../img/$icon");
+		move_uploaded_file($photo_tmp, "../img/$photo");
 
-        move_uploaded_file($icon_tmp, "../img/$icon");
-        move_uploaded_file($photo_tmp, "../img/$photo");
+		if(empty($icon)){
+			$query = "SELECT * FROM services_tbl WHERE id = ?";
 
-        if(empty($icon)){
-			$query = "SELECT * FROM services_tbl WHERE id = '$id'";
+			$stmt = mysqli_prepare($connection, $query);
+			mysqli_stmt_bind_param($stmt, "i", $id);
+			mysqli_stmt_execute($stmt);
 
-			$icon_query = mysqli_query($connection, $query);
-
-			while($row = mysqli_fetch_array($icon_query)){
+			$result = mysqli_stmt_get_result($stmt);
+			while($row = mysqli_fetch_array($result)){
 				$icon = $row['icon'];
 			}
 		}
 
 		if(empty($photo)){
-			$query = "SELECT * FROM services_tbl WHERE id = $id";
+			$query = "SELECT * FROM services_tbl WHERE id = ?";
 
-			$photo_query = mysqli_query($connection, $query);
+			$stmt = mysqli_prepare($connection, $query);
+			mysqli_stmt_bind_param($stmt, "i", $id);
+			mysqli_stmt_execute($stmt);
 
-			while($row = mysqli_fetch_array($photo_query)){
+			$result = mysqli_stmt_get_result($stmt);
+			while($row = mysqli_fetch_array($result)){
 				$photo = $row['photo'];
 			}
 		}
 
 		$query = "UPDATE services_tbl SET ";
-		$query .= "icon = '$icon', ";
-		$query .= "title = '$title', ";
-		$query .= "content = '"$content"', ";
-		$query .= "button_text = '$button_text', ";
-		$query .= "button_link = '$button_link', ";
-		$query .= "icon = '$icon', ";
-		$query .= "photo = '$photo', ";
-		$query .= "status = '$status' ";
-		$query .= "WHERE id = $id";
+		$query .= "icon = ?, ";
+		$query .= "title = ?, ";
+		$query .= "content = ?, ";
+		$query .= "button_text = ?, ";
+		$query .= "button_link = ?, ";
+		$query .= "icon = ?, ";
+		$query .= "photo = ?, ";
+		$query .= "status = ? ";
+		$query .= "WHERE id = ?";
 
-		$update_query = mysqli_query($connection, $query);
+		$stmt = mysqli_prepare($connection, $query);
+		mysqli_stmt_bind_param($stmt, "ssssssssi", $icon, $title, $content, $button_text, $button_link, $icon, $photo, $status, $id);
+		$update_query = mysqli_stmt_execute($stmt);
 
 		if(!$update_query){
 			die("QUERY FAILED". mysqli_error($connection));
 		}
 
-
 		//header("Location: ../index.php");
 		$confirm_message = "Service Successfully Updated.";
-        $loc = "../services.php?source=services&id=".$id;
-        echo "<script type='text/javascript'>alert('$confirm_message'); location.href='$loc'; </script>";
-
-
+		$loc = "../services.php?source=services&id=".$id;
+		echo "<script type='text/javascript'>alert('$confirm_message'); location.href='$loc'; </script>";
 	}
 ?>
