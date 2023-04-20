420e9e32af1cf1c735985d7eaecdfe07694b7752
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-20 11:52:42 +0800

    Update services-report.php

--- editor/services-report.php
+++ editor/services-report.php
@@ -290,7 +290,7 @@
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
 
-  
+
 <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=640c46a109bfca551c61da47" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="../js/webflow.js" type="text/javascript"></script>
 <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
@@ -570,6 +570,7 @@ $(document).ready(function() {
 
 function loadPrint() {
 
+alert("Click loadprint");
   const jsPDF = window.jspdf.jsPDF;
   const downloadButton = document.querySelector('#download-pdf');
 
