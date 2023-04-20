2d74e14f4812af14dd90c853b0bd6b2ae9d9d5ac
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-20 11:16:24 +0800

    Update create-certificate.php

--- editor/create-certificate.php
+++ editor/create-certificate.php
@@ -624,23 +624,23 @@
       printDiv();
     });
     function printDiv() {
-  //var cert_type = "my-cert-type"; // Replace with your actual element ID
+      //var cert_type = "my-cert-type"; // Replace with your actual element ID
       var printContents = document.getElementById(cert_type).innerHTML;
       var originalContents = document.body.innerHTML;
       document.body.innerHTML = printContents;
       window.print();
       document.body.innerHTML = originalContents;
-  // Add event listener for afterprint
+        // Add event listener for afterprint
       window.addEventListener("afterprint", function(event) {
         console.log("afterprint event:", event);
         setTimeout(function() {
       location.reload(); // Reload the page after a delay
     }, 50); // Delay in milliseconds (adjust as needed)
       }, false);
-  // Fallback for browsers that don't support afterprint
-      setTimeout(function() {
-        location.reload();
-  }, 100); // Reload the page after a delay (adjust as needed)
+      // Fallback for browsers that don't support afterprint
+      //     setTimeout(function() {
+      //       location.reload();
+      // }, 100); // Reload the page after a delay (adjust as needed)
     }
 
 
