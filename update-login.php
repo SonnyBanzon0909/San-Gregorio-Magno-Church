6426f6a8b91f2425b9ef97ed7b3ff4f6805e781b
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 02:07:31 +0800

    Update update-login.php

--- update-login.php
+++ update-login.php
@@ -1,22 +1,31 @@
 <?php
-// Set the values of the parameters based on the form input
-$old_logo = "http://parokya-ni-san-gregorio-magno.com/images/".$_POST['oldphoto'];
-$logo = $_POST['photo'];
-$full_name = $_POST['Full-Name-3'];
-$birthday = $_POST['Birthday-2'];
-$contact_number = $_POST['Contact-number-2'];
-$address = $_POST['Address-3'];
-$barangay = $_POST['Barangay'];
+// Start the session
+session_start();
+require_once "connect.php";
 
-// Check if $logo is empty, if so, use the old logo
-if (empty($logo)) {
-    $logo = $old_logo;
+// Retrieve the email value from the session
+if (isset($_SESSION['user_email'])) {
+    $email = $_SESSION['user_email'];
+} else {
+    // handle case where email is not set
 }
 
 // Build the SQL query with a prepared statement to prevent SQL injection
 $sql = "UPDATE login SET logo=?, fullname=?, birthday=?, contact=?, address=?, barangay=? WHERE email=?";
 $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "sssssss", $logo, $full_name, $birthday, $contact_number, $address, $barangay, $email);
+
+// Set the values of the parameters based on the form input
+$old_logo = "http://parokya-ni-san-gregorio-magno.com/images/".$_POST['oldphoto'];
+$logo = $_POST['photo'];
+if (empty($logo)) {
+    $logo = $old_logo;
+}
+$full_name = $_POST['Full-Name-3'];
+$birthday = $_POST['Birthday-2'];
+$contact_number = $_POST['Contact-number-2'];
+$address = $_POST['Address-3'];
+$barangay = $_POST['Barangay'];
 
 // Execute the query and check for errors
 if (mysqli_stmt_execute($stmt)) {
@@ -29,5 +38,4 @@ if (mysqli_stmt_execute($stmt)) {
 // Close the statement and connection
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
-
 ?>
