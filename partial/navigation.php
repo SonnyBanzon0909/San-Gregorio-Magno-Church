fee6387431fcbefeb95a312c101534e5879b67aa
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 02:38:09 +0800

    Update navigation.php

--- partial/navigation.php
+++ partial/navigation.php
@@ -200,9 +200,23 @@ while($row = mysqli_fetch_array($view_query)){
       <!--  -->
       <div data-hover="false" id="profile" data-delay="500" data-w-id="a3e29982-56b0-b8b9-18b8-0f02a2630aa4" class="dropdown w-dropdown profile" style="width: 100%;">
         <div class="dropdown-toggle w-dropdown-toggle" style="width: 100%; padding-bottom: 15px;">
-          <div class="profile-img-con" style="margin-right: 23px;">
-            <div class="profile-initial" style="text-transform: uppercase;">E</div>
-          </div><img src="images/down-chevron.svg" loading="lazy" alt="" class="drop-icon">
+          <div class="profile-img-con" style="margin-right: 23px;position: relative;">
+            <?php if (!empty($photo)) { ?>
+              <div class="profile-initial" style="display: none;">s</div>
+              <img id="profile" src="http://parokya-ni-san-gregorio-magno.com/images/<?php echo $photo; ?>" style="
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              border-radius: 100%;
+              ">
+            <?php } else { ?>
+              <div class="profile-initial" style="text-transform: uppercase;">s</div>
+              <img id="profile" src="" style="display: none;">
+            <?php } ?>
+          </div>
+
+          <img src="images/down-chevron.svg" loading="lazy" alt="" class="drop-icon">
         </div>
         <nav class="dropdown-list w-dropdown-list" style="width:100% !important; min-width: 250px;">
           <div class="login-content-con">
