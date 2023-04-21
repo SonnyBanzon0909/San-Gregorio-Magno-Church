84d134c33ad575724a870f78f3d9d49ab03bb7e9
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 01:08:39 +0800

    Update update-photo.php

--- update-photo.php
+++ update-photo.php
@@ -11,19 +11,24 @@ if (isset($_SESSION['user_email'])) {
     // Handle the case where the email value is not set
 }
 
+// Get the filename and file path of the uploaded logo
+$logo_filename = $_FILES['file']['name'];
+$logo_filepath = "images/" . $logo_filename;
+
 // Build the SQL query with a prepared statement to prevent SQL injection
 $sql = "UPDATE login SET logo=? WHERE email=?";
 $stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "ss", $logo, $email);
+mysqli_stmt_bind_param($stmt, "ss", $logo_filename, $email);
 
 // Set the values of the parameters based on the form input
-$logo = $_FILES['file']['name']; // Use the file name as the logo value
-if (mysqli_stmt_execute($stmt)) {
-    // Handle the case where the SQL query was executed successfully
+$logo = $_FILES['file']['tmp_name']; // Use the temporary file path as the logo value
+
+if (move_uploaded_file($logo, $logo_filepath) && mysqli_stmt_execute($stmt)) {
+    // Handle the case where the file was uploaded and SQL query was executed successfully
     echo "success";
 } else {
-    // Handle the case where the SQL query failed to execute
-    echo "Logo update failed";
+    // Handle the case where either the file upload or SQL query failed
+    echo "Logo upload or update failed";
 }
 
 // Close the statement and connection
