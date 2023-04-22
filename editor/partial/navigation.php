a739cee6fe7788535777627c2c906573ce855a87
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 00:18:02 +0800

    Update navigation.php

--- editor/partial/navigation.php
+++ editor/partial/navigation.php
@@ -69,6 +69,8 @@
               <a id="w-node-_14e56277-239e-cc10-6aba-00a1806977e7-806977bd" href="request-certificate.php" class="link">Request Certificate</a>
               <a id="w-node-_14e56277-239e-cc10-6aba-00a1806977e7-806977bd" href="services-report.php" class="link">Services Report</a>
               <a id="w-node-_14e56277-239e-cc10-6aba-00a1806977e7-806977bd" href="priests.php" class="link">Priests</a>
+              <a id="w-node-_14e56277-239e-cc10-6aba-00a1806977e7-806977bd" href="users.php" class="link">Add User</a>
+              
             </div>
           </nav>
         </div>
