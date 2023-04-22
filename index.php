020a9c6a470a3d2b28b024898dfe61913bed4354
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 22:51:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ else
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
  
-    <link href="images/new-favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="images/new-favicon.ico?v=2" rel="shortcut icon" type="image/x-icon">
     <link href="images/webclip.png" rel="apple-touch-icon">
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-XREV9R6ECX"></script>
     <script type="text/javascript">window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('set', 'developer_id.dZGVlNj', true);gtag('config', 'G-XREV9R6ECX');</script>
