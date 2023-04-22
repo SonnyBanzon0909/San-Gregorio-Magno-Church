8f162a3ad6783e03ec1b6262db5c496d20961b24
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 21:51:33 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,14 +5,15 @@ require_once "connect.php";
 $email = mysqli_real_escape_string($conn, $_POST['l-email']);
 $password = mysqli_real_escape_string($conn, $_POST['l-password']);
 
-// Step 1: Retrieve the hashed password from the database
-$sql = "SELECT password FROM login WHERE email = '$email'";
+// Step 1: Retrieve the hashed password and logo from the database
+$sql = "SELECT password, logo FROM login WHERE email = '$email'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  // Retrieve the hashed password from the database
+  // Retrieve the hashed password and logo from the database
   $row = mysqli_fetch_assoc($result);
   $hashedPassword = $row['password'];
+  $logo = $row['logo'];
 
   // Step 2: Verify the user's password
   if (password_verify($password, $hashedPassword)) {
@@ -20,6 +21,11 @@ if (mysqli_num_rows($result) > 0) {
     session_start();
     $_SESSION['login_account'] = "true";
     $_SESSION['user_email'] = $email;
+
+    // Set the cookies
+    setcookie("cookie_email", $email, time() + (30 * 24 * 60 * 60));
+    setcookie("cookie_password", $password, time() + (30 * 24 * 60 * 60));
+    setcookie("cookie_image", $logo, time() + (30 * 24 * 60 * 60));
 
     echo "success";
   } else {
