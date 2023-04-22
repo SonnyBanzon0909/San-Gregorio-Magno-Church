48405d21217a8420c3f48593ad5649b4b6942533
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-22 17:22:08 +0800

    Update otp.php

--- otp.php
+++ otp.php
@@ -80,7 +80,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'mail.parokya-ni-san-gregorio-magno.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'noreply@parokya-ni-san-gregorio-magno.com';      //SMTP username
+    $mail->Username   = 'admin@parokya-ni-san-gregorio-magno.com';      //SMTP username
     $mail->Password   = 'l)UKks28.Jp67H';                      //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
     $mail->Port       = 465;     
