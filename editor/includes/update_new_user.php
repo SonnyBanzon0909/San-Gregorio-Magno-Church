41a59372a8db4e141ec88cf20b1d71aa60e8e200
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 00:33:26 +0800

    Update update_new_user.php

--- editor/includes/update_new_user.php
+++ editor/includes/update_new_user.php
@@ -1,30 +1,27 @@
 <?php 
-	include "db.php";
+include "db.php";
 
-	if(isset($_POST['update_user'])){
-		$id = $_POST['id'];
-		$username = $_POST['username'];
-        $password = $_POST['password'];
-        $role = $_POST['role'];
+if(isset($_POST['update_user'])){
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$role = $_POST['role'];
 
-		$query = "UPDATE admin_users SET ";
-		$query .= "username = '$username', ";
-		$role .= "role = '$role', ";
-		$query .= "password = '$password' ";
-		$query .= "WHERE id = $id";
+	$query = "UPDATE admin_users SET ";
+	$query .= "username = '$username', ";
+	$query .= "role = '$role', ";
+	$query .= "password = '$password' ";
+	$query .= "WHERE id = $id";
 
-		$update_query = mysqli_query($connection, $query);
+	$update_query = mysqli_query($connection, $query);
 
-		if(!$update_query){
-			die("QUERY FAILED". mysqli_error($connection));
-		}
-
-
-		//header("Location: ../index.php");
-		$confirm_message = "Record Successfully Updated.";
-        $loc = "../users.php";
-        echo "<script type='text/javascript'>alert('$confirm_message'); location.href='$loc'; </script>";
-
-
+	if(!$update_query){
+		die("QUERY FAILED". mysqli_error($connection));
 	}
+
+	$confirm_message = "Record Successfully Updated.";
+	$loc = "../users.php";
+	echo "<script type='text/javascript'>alert('$confirm_message'); location.href='$loc'; </script>";
+}
+
 ?>
