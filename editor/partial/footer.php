81ab409c53edf5044ebf3bda3933a55a37e5e1f0
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 21:30:25 +0800

    Update footer.php

--- editor/partial/footer.php
+++ editor/partial/footer.php
@@ -44,7 +44,7 @@
       <div class="tawkto-wrapper">
         <div class="tawkto-embed w-embed w-script">
           <!-- Start of Tawk.to Script -->
-          <script type="text/javascript">
+         <!--  <script type="text/javascript">
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -54,7 +54,7 @@
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
             })();
-          </script>
+          </script> -->
           <!-- End of Tawk.to Script -->
         </div>
       </div>
