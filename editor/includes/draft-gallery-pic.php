7f733d195b4f5083d0bf68c8018dd1083f08900a
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 19:47:09 +0800

    cx

--- editor/includes/draft-gallery-pic.php
+++ editor/includes/draft-gallery-pic.php
@@ -1,16 +1,15 @@
 <?php
 // Get the ID parameter from the request
 $id = $_GET['id'];
- ob_start();
-  include "db.php";
-
+include "db.php";
 
 try {
- 
   $stmt = $connection->prepare('UPDATE gallery_images_tbl SET status = "Draft" WHERE id = :id');
   $stmt->execute(array('id' => $id));
   echo 'Success'; // Send a success response back to the client
 } catch(PDOException $e) {
   echo 'Error: ' . $e->getMessage(); // Send an error response back to the client
 }
+
+ 
 ?>
