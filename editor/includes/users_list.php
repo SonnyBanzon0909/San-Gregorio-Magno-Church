d391a0db7b2d6ac966eeb18545a4606daaa7263d
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 22:04:18 +0800

    Delete User

--- editor/includes/users_list.php
+++ editor/includes/users_list.php
@@ -1,58 +1,65 @@
   
 <?php
-    
-    if(isset($_GET['id'])){
-    $id = $_GET['id'];
+
+if(isset($_GET['id'])){
+  $id = $_GET['id'];
 
 
-    $query = "SELECT * FROM admin_users WHERE id = $id";
-    $view_query = mysqli_query($connection, $query);
+  $query = "SELECT * FROM admin_users WHERE id = $id";
+  $view_query = mysqli_query($connection, $query);
 
-    if(!$view_query){
-      die("QUERY FAILED". mysqli_error($connection));
-    }
-
-    while($row = mysqli_fetch_array($view_query)){
-      $id = $row["id"];
-      $username = $row["username"];
-      $password = $row["password"];
-      $role = $row["role"];
-    }
+  if(!$view_query){
+    die("QUERY FAILED". mysqli_error($connection));
   }
+
+  while($row = mysqli_fetch_array($view_query)){
+    $id = $row["id"];
+    $username = $row["username"];
+    $password = $row["password"];
+    $role = $row["role"];
+  }
+}
 
 ?>
 
 
 
-  <div class="section contact-section wf-section">
-    <div class="container">
-      <div class="form-grid">
-        <div id="w-node-_7a260c29-eaea-38ca-7dd7-75753354bfb4-f1521fbf" data-w-id="7a260c29-eaea-38ca-7dd7-75753354bfb4" style="opacity:1" class="form-con-wrappera">
-          <div class="form-container">
-            <div class="form-title">Users</div>
-            <div class="w-form">
-              
-                <div class="field-container">
-                  <?php 
-                    $query = "SELECT * FROM admin_users";
-                    $select_all_users = mysqli_query($connection, $query);
-                    while($row = mysqli_fetch_array($select_all_users)){
-                      $id = $row['id'];
-                      $username = $row['username'];
+<div class="section contact-section wf-section">
+  <div class="container">
+    <div class="form-grid">
+      <div id="w-node-_7a260c29-eaea-38ca-7dd7-75753354bfb4-f1521fbf" data-w-id="7a260c29-eaea-38ca-7dd7-75753354bfb4" style="opacity:1" class="form-con-wrappera">
+        <div class="form-container">
+          <div class="form-title">Users</div>
+          <div class="w-form">
 
-                      echo '<div class="announcement_wrapper"><a href="users.php?source=update_user&id='.$id.'">'.$username.'</a></div>';
+            <div class="field-container">
+              <?php 
+              $query = "SELECT * FROM admin_users";
+              $select_all_users = mysqli_query($connection, $query);
+              while($row = mysqli_fetch_array($select_all_users)){
+                $id = $row['id'];
+                $username = $row['username'];
 
-                    }
-                  ?>
-                  <div class="">
-                  </div>
-                </div>
+                echo '
+                <div id ="galcon-'.$id.'" style="position: relative; padding-right: 30px; display: flex; align-items: center">
 
-                <div data-w-id="5b8a60ca-0674-a70c-a128-7036f4b41cbe" class="button rounded-button">
-                  <div style="opacity:0" class="button-overlay"></div>
-                  <a href="users.php?source=add_user"><button class="button-link w-button">Add New User</button></a>
-                </div>
-              
+                <img class="remove-pic" src="images/close-icon-2.svg" style="position: absolute; top: auto; left: auto; right: 0px; bottom: auto; cursor: pointer;">
+
+                <div class="announcement_wrapper" style="width: 100% !important;" ><a href="users.php?source=update_user&id='.$id.'">'.$username.'</a></div>
+
+                </div>';
+
+              }
+              ?>
+              <div class="">
+              </div>
+            </div>
+
+            <div data-w-id="5b8a60ca-0674-a70c-a128-7036f4b41cbe" class="button rounded-button">
+              <div style="opacity:0" class="button-overlay"></div>
+              <a href="users.php?source=add_user"><button class="button-link w-button">Add New User</button></a>
+            </div>
+
               <!-- <div class="w-form-done">
                 <div>Thank you! Your submission has been received!</div>
               </div>
