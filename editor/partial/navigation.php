dae7f1d3cbc56ce31b351f9b2505a05723a297a2
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 21:36:00 +0800

    Update navigation.php

--- editor/partial/navigation.php
+++ editor/partial/navigation.php
@@ -20,7 +20,7 @@ while($row = mysqli_fetch_array($user_query)){
 
 ?>
 <style>
-  .tawkto-wrapper, #upbqpi68nkog1682256762967.widget-visible 
+  .tawkto-wrapper, .widget-visible 
   {
     display: none !important;
   }
