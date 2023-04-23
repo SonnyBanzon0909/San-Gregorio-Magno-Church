2cc1bd6f87db6ea90cff8fce84cd5035ac6afae8
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 22:39:10 +0800

    Update request.php

--- request.php
+++ request.php
@@ -52,7 +52,33 @@ if($user!="")
 
 
 
-} else {
+// Start of Email
+        
+
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+        require 'phpmailer/vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+
+
+
+///
+
+
+
+// END of Email
+
+
+
+
+} else 
+{
     //echo "Error: " . $stmt->error;
 }
 
