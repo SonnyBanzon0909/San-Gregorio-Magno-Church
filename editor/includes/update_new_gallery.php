26f708dc981710f6294e910d2e6c4b386d22fa66
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 23:52:16 +0800

    sd

--- editor/includes/update_new_gallery.php
+++ editor/includes/update_new_gallery.php
@@ -7,37 +7,8 @@ if(isset($_POST['update_gallery'])){
 	$gallery_date = $_POST['gallery_date'];
 	$details = $_POST['details'];
 	$status = $_POST['status'];
-
-        // $icon = $_FILES['icon']['name'];
-        // $icon_tmp = $_FILES['icon']['tmp_name'];
-
-        // $photo = $_FILES['photo']['name'];
-        // $photo_tmp = $_FILES['photo']['tmp_name'];
-
-        //$post_comment_count = 4;
-
-        // move_uploaded_file($icon_tmp, "../img/$icon");
-        // move_uploaded_file($photo_tmp, "../img/$photo");
-
-        // if(empty($icon)){
-		// 	$query = "SELECT * FROM services_tbl WHERE id = $id";
-
-		// 	$icon_query = mysqli_query($connection, $query);
-
-		// 	while($row = mysqli_fetch_array($icon_query)){
-		// 		$icon = $row['icon'];
-		// 	}
-		// }
-
-		// if(empty($photo)){
-		// 	$query = "SELECT * FROM services_tbl WHERE id = $id";
-
-		// 	$photo_query = mysqli_query($connection, $query);
-
-		// 	while($row = mysqli_fetch_array($photo_query)){
-		// 		$photo = $row['photo'];
-		// 	}
-		// }
+echo $status;
+        
 
 	$query = "UPDATE gallery_tbl SET ";
 	$query .= "title = '$title', ";
