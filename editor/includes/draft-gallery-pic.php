74807732e463f6a13a6a52f2b7683461886b5594
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 20:05:01 +0800

    Update draft-gallery-pic.php

--- editor/includes/draft-gallery-pic.php
+++ editor/includes/draft-gallery-pic.php
@@ -4,15 +4,12 @@ $picid = $_GET['id'];
 include "db.php";
  
 try {
-  echo "O hell no";
   $stmt = $connection->prepare('UPDATE gallery_images_tbl SET status = "Draft" WHERE id = :picid');
-  $stmt->execute(array('id' => $picid));
+  $stmt->execute(array('picid' => $picid));
   echo 'Success'; // Send a success response back to the client
 } 
 catch(PDOException $e) 
 {
   echo 'Error: ' . $e->getMessage(); // Send an error response back to the client
 }
-
- 
 ?>
