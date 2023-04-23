3eaab4ad55481f359946b501a0dd9c01616f0d27
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-24 02:51:06 +0800

    update services

---
+++ editor/update-service-form.php
@@ -0,0 +1,25 @@
+<?php
+// assuming you have already established a connection to the database
+// $conn = mysqli_connect("localhost", "username", "password", "database_name");
+ob_start();
+include "includes/db.php";
+$id = $_POST['id'];
+
+
+if (isset($_POST['update_contact'])) {
+  $id = $_GET['id']; // assuming the id is passed through the URL
+  $name = $_POST['name'];
+  $gender = $_POST['gender'];
+  $age = $_POST['age'];
+  $barangay = $_POST['barangay'];
+  $date_time = $_POST['date'];
+
+  $query = "UPDATE formdata SET name='$name', gender='$gender', age='$age', barangay='$barangay', date_time='$date_time' WHERE id=$id";
+
+  if(mysqli_query($conn, $query)) {
+    echo "Record updated successfully.";
+  } else {
+    echo "Error updating record: " . mysqli_error($conn);
+  }
+}
+?>
