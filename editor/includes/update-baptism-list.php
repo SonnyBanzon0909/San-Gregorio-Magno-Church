7d4447461de1e7926c65021e4d8434fed7225b89
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-24 02:14:30 +0800

    Update update-baptism-list.php

--- editor/includes/update-baptism-list.php
+++ editor/includes/update-baptism-list.php
@@ -85,8 +85,8 @@ while($row = mysqli_fetch_array($user_query)){
   </head>
   <body>
 
-    <?php include 'partial/custom-css.html'; ?>
-    <?php include 'partial/navigation.php'; ?>
+    <?php include '../partial/custom-css.html'; ?>
+    <?php include '../partial/navigation.php'; ?>
 
     <!-- Start of Baptism -->
 
@@ -287,7 +287,7 @@ while($row = mysqli_fetch_array($user_query)){
   <!-- End of Baptism -->
 
 
-  <?php include 'partial/footer.php'; ?>
+  <?php include '../partial/footer.php'; ?>
 
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=640c46a109bfca551c61da47" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
