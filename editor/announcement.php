1c6598ae930f1576d87a7369e49a6b615be2e74f
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-23 21:04:19 +0800

    sd

--- editor/announcement.php
+++ editor/announcement.php
@@ -303,7 +303,7 @@ while($row = mysqli_fetch_array($user_query)){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        if (this.responseText=='Sucess') 
+        if (this.responseText=='Success') 
         {
           alert("Deleted");
         }
