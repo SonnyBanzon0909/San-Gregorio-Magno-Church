1de11e51af7c43d51c9435e66383bbcbde0f96b1
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-24 03:14:07 +0800

    Update update-service-form.php

--- editor/update-service-form.php
+++ editor/update-service-form.php
@@ -6,13 +6,19 @@ include "includes/db.php";
 $id = $_GET['id'];
 
 
-// if (isset($_POST['update_contact'])) {
+if (isset($_POST['update_contact'])) {
   // $id = $_GET['id']; // assuming the id is passed through the URL
   $name = $_POST['name'];
   $gender = $_POST['gender'];
   $age = $_POST['age'];
   $barangay = $_POST['barangay'];
   $date_time = $_POST['date'];
+
+echo $name;
+echo $gender;
+echo $age;
+echo $barangay;
+echo $date_time;
 
   $query = "UPDATE formdata SET name='$name', gender='$gender', age='$age', barangay='$barangay', date_time='$date_time' WHERE id='$id'";
 
@@ -21,5 +27,5 @@ $id = $_GET['id'];
   } else {
     echo "Error updating record: " . mysqli_error($conn);
   }
-// }
+}
 ?>
