596c93c10a17d1f396c43b66c467a38491abcac2
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-24 00:16:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ else
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 
     <link href="http://parokya-ni-san-gregorio-magno.com/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
-    <link href="images/webclip.png" rel="apple-touch-icon">
+    <link href="ttp://parokya-ni-san-gregorio-magno.com/images/favicon.ico" rel="apple-touch-icon">
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-XREV9R6ECX"></script>
     <script type="text/javascript">window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('set', 'developer_id.dZGVlNj', true);gtag('config', 'G-XREV9R6ECX');</script>
     <style>
