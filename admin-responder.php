2f444c4ac3f717454fd5576f7b52f5228721cdc0
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-04-24 15:24:22 +0800

    Update admin-responder.php

--- admin-responder.php
+++ admin-responder.php
@@ -8,8 +8,8 @@ session_start();
 require_once "connect.php";
 
 /// Email
- 
- $email = "sangregoriomagnochurch@gmail.com"; 
+
+$email = "sangregoriomagnochurch@gmail.com"; 
 
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -22,7 +22,7 @@ require 'phpmailer/vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
- 
+
 
 
 
@@ -38,7 +38,7 @@ try {
     $mail->Password   = 'l)UKks28.Jp67H';                      //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
     $mail->Port       = 465;     
- 
+
     //Recipients
     $mail->setFrom('admin@parokya-ni-san-gregorio-magno.com', 'San Gregorio Magno');
     $mail->addAddress($email);            //Add a recipient
@@ -48,26 +48,27 @@ try {
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'You have a new form submission';
 
-    $mail->Body    = 'Dear User,<br><br>
+    $mail->Body    = 'Dear Admin,
 
-    Thank you for using our service. As requested, please find your one-time password (OTP) below:<br><br>
+    You have received a new form submission on your website. The details are as follows:
 
-    OTP: <br><br>
+    Name: [Name]
+    Email: [Email]
+    Message: [Message]
 
-    This OTP is valid for 5 Minutes and can only be used once. Please enter the OTP code on our website/app to complete the login/sign-up process.<br><br>
-
-    If you did not request an OTP, please ignore this email and contact us immediately if you suspect any unauthorized activity on your account.<br><br>
+    Please take a look at the message and respond to the user as soon as possible.
+    <br><br>
 
     Best regards,<br>
     Admin<br>
     Parokya ni San Gregorio Magno<br><br>
     ';
 
-    $mail->AltBody = 'Your OTP is: ' . $otp . '. This OTP is valid for 5 Minutes and can only be used once. Please enter the OTP code on our website/app to complete the login/sign-up process.';
+    $mail->AltBody = 'You have received a new form submission on your website.';
 
     $mail->send();
 
-    echo '<script>alert("Message has been sent! \nNote: If you did not receive this email, please check your spam folder.");</script>';
+    //echo '<script>alert("Message has been sent! \nNote: If you did not receive this email, please check your spam folder.");</script>';
 
 
 
