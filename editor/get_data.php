2c23da848b0a6746c28ce093f44009989079a2e0
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-05-01 00:52:46 +0800

    sd

--- editor/get_data.php
+++ editor/get_data.php
@@ -30,7 +30,7 @@ while ($row = mysqli_fetch_assoc($dis_result)) {
   $num_results = mysqli_num_rows($result);
   //echo '<h1>'.$num_results.'</h1>';
   echo '<div id="w-node-fd420557-f27d-1ee9-638f-2ed0318bf2a4-e3ad4e69" class="admin-service-item">';
-  echo '<div class="logo-wrapper"><img src="../images/Baptism-1.png" loading="lazy" alt="" class="admin-service-logo"></div>';
+  echo '<div class="logo-wrapper"><img src="../images/service.svg" loading="lazy" alt="" class="admin-service-logo"></div>';
   echo '<div class="admin-service-card">';
   echo '<div class="serv-card-line-wrapper">';
   echo '<div class="serv-card-title">' . $purpose . '</div>';
