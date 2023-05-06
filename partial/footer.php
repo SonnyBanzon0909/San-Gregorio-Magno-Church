72ab64548560814bc8bd38a872c43e70740d7ba1
SonnyBanzon0909 <70322931+SonnyBanzon0909@users.noreply.github.com>
2023-05-06 21:26:05 +0800

    Update footer.php

--- partial/footer.php
+++ partial/footer.php
@@ -113,7 +113,7 @@ if (isset($_SESSION['user_email'])) {
         <div class="input-con"><input required type="text" class="input-field w-input create-field" maxlength="256" name="c-fullname" data-name="Full Name 3" placeholder="Full Name" id="c-fullname"></div>
 
         <div class="input-con">
-          <input required type="date" class="input-field w-input create-field" maxlength="256" name="c-birthday" data-name="Birthday 2" placeholder="Birthday" id="c-birthday" min="<?= date('Y-m-d') ?>">
+          <input required type="text" class="input-field w-input create-field" maxlength="256" name="c-birthday" data-name="Birthday 2" placeholder="Birthday" id="c-birthday" min="<?= date('Y-m-d') ?>">
           <img src="images/calendar-2.svg" loading="lazy" alt="" class="calendar">
         </div>
 
